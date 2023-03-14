b959a41fb84182daaf9a7ff5afb60d6ca0644571
Drew H <60757324+thedrewhall@users.noreply.github.com>
2023-03-14 15:25:43 -0400

    Updated Pay Calculator
    Added Variable Tax Rate and adjusted spacing

--- Pay Calculator.py
+++ Pay Calculator.py
@@ -1,16 +1,30 @@
-# Pay Calculator
+# Adjustable Inputs Allow Flexible Calculations
 
-hrs = input("Number of Hours Per Week?")
-rate = input("Pay Rate?")
-weekly_gross_pay = float(hrs) * float(rate)
-yearly_gross_pay = float(hrs) * float(rate) * 52
+print(" ")
+hrs = input("Estimated Hours Per Week: ")
+print(" ")
+p_rate = input("Pay Rate: ")
+print(" ")
+t_rate = input("Tax Rate: ")
+
+
+# Gross Pay Calculations
+
+adjusted_t_rate = int(t_rate) / 100
+weekly_gross_pay = int(hrs) * int(p_rate)
+yearly_gross_pay = int(hrs) * int(p_rate) * 52
 monthly_gross_pay = yearly_gross_pay / 12
 
-# Taxes calculated assuming an average baseline 15% tax rate
-weekly_net_pay = int(weekly_gross_pay) * .85
-monthly_net_pay = int(monthly_gross_pay) * .85
-yearly_net_pay = int(yearly_gross_pay) * .85
 
+# Net Pay Calculations
+
+weekly_net_pay = round(weekly_gross_pay * (1-adjusted_t_rate), 2 )
+monthly_net_pay = round(monthly_gross_pay * (1-adjusted_t_rate), 2 )
+yearly_net_pay = round(yearly_gross_pay * (1-adjusted_t_rate), 2 )
+
+## print(adjusted_t_rate)
+print(" ")
+print("~~~~~~~~")
 print(" ")
 print("Pay Per Week:")
 print("Gross: $"+str(weekly_gross_pay))
@@ -23,3 +37,4 @@ print(" ")
 print("Pay Per Year:")
 print("Gross: $"+str(yearly_gross_pay))
 print("After Estimated Taxes: $"+str(yearly_net_pay))
+print(" ")
